a94699f81cb1fdd4008c96ada852b18b0974a1fe
Austin Spaeth <austinspaeth@msn.com>
2021-03-12 20:35:19 -0800

    Finished the header component and added it to the different views.

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,4 +1,4 @@
-const data = {
+export const data = {
     "site": {
         "title": "Earthquake Zen Garden",
         "heroImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Arenal_Volcano_07_2015_CRI_3828. jpg / 1920 px - Arenal_Volcano_07_2015_CRI_3828.jpg ",
